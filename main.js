8ff8b5188b423ca9560347be6d88c332be613c20
SuperVK <victor.klomp@gmail.com>
2018-08-19 11:53:17 +0200

    added something that keeps the bot alive

--- main.js
+++ main.js
@@ -48,3 +48,8 @@ client.on('messageCreate', async (message) => {
 })
 
 client.connect()
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://fountain-bot.herokuapp.com");
+}, 300000); // every 5 minutes (300000)

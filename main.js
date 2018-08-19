4fde3ce3cdffeec23f497971afa3aea4f6bc5b39
SuperVK <victor.klomp@gmail.com>
2018-08-19 11:27:57 +0200

    Added heroku support and setip

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const Eris = require('eris')
 const fetch = require('node-fetch')
-const config = require('./config.json')
-const PREFIX = '!'
-//const IP = 'FountainGames.aternos.me'
-const IP = 'mc.hypixel.net'
-const client = new Eris(config.token)
+let token, prefix
+if(require('./config.json') != undefined) {
+    token = require('./config.json').token
+    prefix = require('./config.json').prefix
+} else {
+    token = process.env.TOKEN
+    prefix = process.env.PREFIX
+}
+let IP = 'FountainGames.aternos.me'
+const client = new Eris(token)
 
 client.on('ready', () => {
     console.log('ready!')
-    //client.getMessage('472481133322305537', '480480949738536984')
 })
 
 client.on('messageReactionAdd', async (message, emoji, userID) => {
@@ -17,16 +21,26 @@ client.on('messageReactionAdd', async (message, emoji, userID) => {
     if(emoji.name == '🔁') {
         let res = await fetch(`https://mcapi.us/server/status?ip=${IP}`)
         let data = await res.json()
-        console.log(data)
-        msg.edit(`Server: ${data.online ? 'online' : 'offline'}\nPlayers: ${data.players.now}/${data.players.max}\nIP: ${IP}`)
+        if(IP.toLowerCase() == 'fountaingames.aternos.me') {
+            msg.edit(`Server: ${!data.motd.startsWith('This server is offline. ') ? 'online' : 'offline'}\nPlayers: ${data.players.now}/${data.players.max}\nIP: ${IP}`)
+        } else {
+            msg.edit(`Server: ${data.online ? 'online' : 'offline'}\nPlayers: ${data.players.now}/${data.players.max}\nIP: ${IP}`)
+        }
     }
 })
 
 client.on('messageCreate', async (message) => {
-    let args = message.content.substring(PREFIX.length).split(' ')
-    switch(args[0]) {
+    if(!message.content.startsWith(prefix)) return
+    let args = message.content.substring(prefix.length).split(' ')
+    switch(args[0].toLowerCase()) {
         case 'ping': {
             message.channel.createMessage('pong!')
+            break;
+        }
+        case 'setip': {
+            if(message.author.id != '235450656335331328' && message.author.id != '333658629582618625') return
+            IP = args[1]
+            message.channel.createMessage('set IP to ' + args[1])
             break;
         }
     }
